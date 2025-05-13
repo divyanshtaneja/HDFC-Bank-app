e9766db85b65ac259ee00050f8b4064617a08bb0
Divyansh Taneja <divyanshtaneja@Divyanshs-MacBook-Air.local>
2025-05-14 01:55:08 +0530

    SKSKS

--- src/components/overview-screen.tsx
+++ src/components/overview-screen.tsx
@@ -26,7 +26,7 @@ export default function OverviewScreen({ onBack, onLogout, onViewSavingsAccount,
   }
 
   const accountTypes = [
-    { name: 'Savings Account', balance: '58,41,024.19', onClick: onViewSavingsAccount },
+    { name: 'Savings Account', balance: '59,01,024.19', onClick: onViewSavingsAccount },
     { name: 'Saving Scheme Account', balance: '17,04,502.00', onClick: onViewSavingSchemeAccount },
     { name: 'Recurring Deposits', balance: null },
     { name: 'My Passion Fund', balance: null },
